3e10477ddfc37feee71e07b3ea76c232b80742de
Otavio Abe <otavioabe@gmail.com>
2024-08-20 19:01:48 -0300

    teste para build

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -7,8 +7,8 @@
     <div class="space-y-6">
         <SectionTitle title='experience.title'/>
         <div class="ml-4">
-            <Skill years='experience.threeYears' measure="w-full" image="public\mdi--language-javascript.png"/>
-            <Skill years='experience.twoYears' measure="w-2/3" image="public\mdi--language-typescript.png"/>
+            <Skill years='experience.threeYears' measure="w-full" image="/mdi--language-javascript.png"/>
+            <Skill years='experience.twoYears' measure="w-2/3" image="/mdi--language-typescript.png"/>
             <Skill years='experience.twoYears' measure="w-2/3" image="public\mdi--vuejs.png"/>
             <Skill years='experience.oneYear' measure="w-1/3" image="public\mdi--react.png"/>
         </div>
